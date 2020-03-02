dd8aea1f292e1a17a9b4a141c08f0522bb4b49f4
Md. Shakil Ahmed <shakilatrai5@gmail.com>
2020-03-02 23:49:18 +0600

    let const practice

---
+++ letconst.js
@@ -0,0 +1,11 @@
+const hubby ="Elius Kanchan";
+console.log(hubby);
+
+let hubby2 = "Shakib Khan";
+
+const number =[10,20,50];
+
+console.log(hubby2);
+number.push(100);
+number.push(10000);
+console.log(number);
